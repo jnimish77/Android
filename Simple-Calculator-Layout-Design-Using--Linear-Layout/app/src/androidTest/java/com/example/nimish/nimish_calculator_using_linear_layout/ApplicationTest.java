20d8db1d330e4e0fe6d06c0375c5e17e97c439b7
Nimish Joshi <jnimish77@gmail.com>
2016-04-09 20:52:26 -0500

    Calculator_using_Linear Layout

---
+++ Simple-Calculator-Layout-Design-Using--Linear-Layout/app/src/androidTest/java/com/example/nimish/nimish_calculator_using_linear_layout/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.nimish.nimish_calculator_using_linear_layout;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
